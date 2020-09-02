fa296faa4a338678bee867cbdb7983a20d251d93
Ólavur Mortensen <olavurmortensen@gmail.com>
2020-09-02 14:12:39 +0000

    Started writing code for unit test. Need to figure out CircleCI, and how I can use it together with Neo4j.

--- pedgraph/test/test.py
+++ pedgraph/test/test.py
@@ -1,3 +1,26 @@
 
 # TODO:
 # Write to DB, read from DB, write to CSV, and compare with original CSV.
+
+from pedgraph.BuildDB import BuildDB
+from pedgraph.WriteCSV import WriteCSV
+import unittest, logging
+from os import environ as env
+
+
+class TestSum(unittest.TestCase):
+
+    def setUp(self):
+        builder = BuildDB(, )
+        writer = WriteCSV(, )
+
+    def test(self):
+        self.assertEqual()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    db_uri = env['NEO4J_BOLT_URI']
+    csv_orig = env['PED_TEST_CSV']
+    csv_rewrite = env['PED_REWRITE_CSV']
+    unittest.main()
+
