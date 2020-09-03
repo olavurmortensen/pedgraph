54c6bf35a2dfeca76a8bdfa61ec937cb585942d2
Ólavur Mortensen <olavurmortensen@gmail.com>
2020-09-03 12:29:20 +0000

    CircleCI now should be able to connect to the DB.

--- pedgraph/test/test.py
+++ pedgraph/test/test.py
@@ -3,11 +3,18 @@
 # TODO:
 # Write to DB, read from DB, write to CSV, and compare with original CSV.
 
+from neo4j import GraphDatabase
 from pedgraph.BuildDB import BuildDB
 from pedgraph.WriteCSV import WriteCSV
 import unittest, logging
 from os import environ as env
 
+# Get the URI to
+NEO4J_URI = env.get('NEO4J_URI')
+
+# If the NEO4J_URI environment variable is not defined, set it to the default.
+if NEO4J_URI is None:
+    NEO4J_URI = 'bolt://localhost:7687'
 
 class TestSum(unittest.TestCase):
 
@@ -16,13 +23,17 @@ class TestSum(unittest.TestCase):
         #builder = BuildDB(, )
         #writer = WriteCSV(, )
 
+    def test_db_connection(self):
+        driver = GraphDatabase.driver(NEO4J_URI)
+        driver.close()
+
     def test(self):
         self.assertEqual(1, 1)
 
+    def tearDown(self):
+        tt = 1
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    #db_uri = env['NEO4J_BOLT_URI']
-    #csv_orig = env['PED_TEST_CSV']
-    #csv_rewrite = env['PED_REWRITE_CSV']
     unittest.main()
 
