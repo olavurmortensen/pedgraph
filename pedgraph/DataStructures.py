d613c7852ef0311b841d7624a86502b06ae9877b
Ólavur Mortensen <olavurmortensen@gmail.com>
2020-09-09 11:30:34 +0100

    Some basic functionality for the Genealogy class. This can be used to reconstruct a genealogy before writing it to CSV. It's probably best if the class itself has a write_csv method.

--- pedgraph/DataStructures.py
+++ pedgraph/DataStructures.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 class Record(object):
-    def __init__(self, ind, father, mother, sex):
+    def __init__(self, ind=None, father=None, mother=None, sex=None):
         self.ind = ind
         self.father = father
         self.mother = mother
@@ -9,9 +13,33 @@ class Record(object):
 
 class Genealogy(object):
     def __init__(self):
-        # TODO
-        tt = 0
+        self.data = dict()
+        self.size = 0
 
-    def get(ind):
-        # TODO
+    def __setitem__(self, key, value):
+        overwrite = False
+        if self.get(key) is not None:
+            logging.warning('Record with ind=%s already exists. Will be over-written.' % key)
+            overwrite = True
+
+        self.data[key] = value
+
+        if not overwrite:
+            self.size += 1
+
+    def __getitem__(self, key):
+        return self.data.get(key)
+
+    def __delitem__(self, key):
+        self.data.__delitem__(key)
+        self.size -= 1
+
+    def get(self, ind):
+        return self[ind]
+
+    def add(self, record):
+        self[record.ind] = record
+
+    def write_csv(self):
+        # Might be a good idea to have the code for writing CSV here instead.
         tt = 0
