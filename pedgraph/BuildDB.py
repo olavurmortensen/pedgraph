da755f23633a2b0fd1c39c91c762ee500394c341
Ólavur Mortensen <olavurmortensen@gmail.com>
2020-09-17 11:50:52 +0000

    Created a BaseBuilder super-class, that generalizes some methods. This class will also be used in a class for creating other node properties from CSV files.

--- pedgraph/BuildDB.py
+++ pedgraph/BuildDB.py
@@ -8,9 +8,97 @@ from tqdm.auto import tqdm
 
 logging.basicConfig(level=logging.INFO)
 
-CSV_FIELDS = ['ind', 'father', 'mother', 'sex']
 
-class BuildDB(object):
+class BaseBuilder(object):
+    '''
+    This base-class contains some methods for basic functionality for building the
+    database.
+    '''
+
+    def __init__(self, uri, csv):
+        '''
+        Arguments:
+        ----------
+        uri :   String
+            URI for the Python Neo4j driver to connect to the database.
+        csv :   String
+            Path to CSV file.
+        '''
+        self.csv = csv
+
+        # Connect to the database.
+        self.driver = GraphDatabase.driver(uri)
+
+    def close(self):
+        # Close the connection to the database.
+        self.driver.close()
+
+    def check_csv_basic(self, csv_columns):
+        '''
+        Check that the CSV (with headers) is not empty, and check that it contains all the necessary
+        columns. Also check that Neo4j is able to access the CSV file.
+        '''
+        # Read the entire CSV using a database query and return all lines.
+        with self.driver.session() as session:
+            result = session.run('LOAD CSV WITH HEADERS FROM $csv AS line RETURN line', csv=self.csv)
+            # If the CSV can't be read, show an error log and raise the error.
+            try:
+                values = result.values()
+            except ClientError:
+                logging.error('CSV file could not be loaded into Neo4j: %s' % self.csv)
+                raise
+
+        assert len(values) > 0, 'Error: reading CSV returned 0 lines.'
+
+        # Get a single row.
+        single = values[0][0]
+        # Get the keys, corresponding to fields in the CSV.
+        keys = list(single.keys())
+
+        logging.info('Loaded CSV with columns: %s' % ','.join(keys))
+
+        # Make sure all the needed fields are in the CSV.
+        for field in csv_columns:
+            assert field in keys, 'Error: CSV does not contain "%s" field.' % field
+
+    def create_index(self, index_name, node_label, node_property, unique=False):
+        '''
+        Create an index on a property on nodes with a specific label, optionally with a
+        uniqueness constraint. Indexing has huge performance benefits.
+
+        Arguments:
+        ----------
+        index_name  :   String
+            Name of index.
+        node_label  :   String
+            Index nodes labelled `(:node_label)`
+        node_property:   String
+            Index property `({node_property})`
+        unique  :   Boolean
+            Whether or not to create a uniqueness constraint on property `node_property`.
+        '''
+        # The index does not exist, so we will create it.
+        with self.driver.session() as session:
+            # Create an index on "ind" and constain it to be unique.
+            try:
+                if unique:
+                    # Create an index with a uniqueness constraint.
+                    result = session.run('CREATE CONSTRAINT %s ON (p:%s) '
+                                         'ASSERT p.%s IS UNIQUE'
+                                         % (index_name, node_label, node_property))
+                    logging.info('Creating an index "%s" on individual IDs.' % index_name)
+                else:
+                    # Create an index.
+                    result = session.run('CREATE INDEX %s FOR (p:%s) ON (p.%s)'
+                                         % (index_name, node_label, node_property))
+            except ClientError as err:
+                if err.title == 'ConstraintAlreadyExists':
+                    logging.info('An index on (:%s {%s}) already exists, will not create.' %(node_label, node_property))
+                else:
+                    raise
+
+
+class BuildDB(BaseBuilder):
     '''
     The constructor connects to the Neo4j database and populates it with people and relations. It
     also checks that there are not duplicate individuals.
@@ -30,31 +118,16 @@ class BuildDB(object):
         self.csv = csv
         self.na_id = na_id
 
+        CSV_COLUMNS = ['ind', 'father', 'mother', 'sex']
+
         # Connect to the database.
         self.driver = GraphDatabase.driver(uri)
 
-
-        # Create a constraint such that the "ind" property is unique.
-        # A constraint at the same time creates an index, which gives faster look-up.
-
-        # Get a list of all database indexes.
-        with self.driver.session() as session:
-            result = session.run('CALL db.indexes')
-            indexes = result.values()
-
-        # If the list contains the 'index_ind' index, we will not create it.
-        index_names = [index[1] for index in indexes]
-        if 'index_ind' not in index_names:
-            logging.info('Creating an index "index_ind" on individual IDs.')
-            # The 'index_ind' index does not exist, so we will create it.
-            with self.driver.session() as session:
-                # Create an index on "ind" and constain it to be unique.
-                result = session.run('CREATE CONSTRAINT index_ind ON (p:Person) ASSERT p.ind IS UNIQUE')
-        else:
-            logging.info('Index "index_ind" already exists, will not create.')
-
         # Check CSV file.
-        self.check_csv()
+        self.check_csv_basic(CSV_COLUMNS)
+
+        # Create an index an uniqueness constraint on 'ind'.
+        self.create_index('index_ind', 'Person', 'ind', unique=True)
 
         # Populate database with nodes (people) and edges (relations).
         self.load_csv()
@@ -69,32 +142,6 @@ class BuildDB(object):
         self.pedstats()
 
         self.close()
-
-    def close(self):
-        # Close the connection to the database.
-        self.driver.close()
-
-    def check_csv(self):
-        # Read the entire CSV using a database query and return all lines.
-        with self.driver.session() as session:
-            result = session.run('LOAD CSV WITH HEADERS FROM $csv AS line RETURN line', csv=self.csv)
-            # If the CSV can't be read, show an error log and raise the error.
-            try:
-                values = result.values()
-            except ClientError:
-                logging.error('CSV file could not be loaded into Neo4j: %s' % self.csv)
-                raise
-
-        assert len(values) > 0, 'Error: reading CSV returned 0 lines.'
-
-        # Get a single row.
-        single = values[0][0]
-        # Get the keys, corresponding to fields in the CSV.
-        keys = list(single.keys())
-
-        # Make sure all the needed fields are in the CSV.
-        for field in CSV_FIELDS:
-            assert field in keys, 'Error: CSV does not contain "%s" field.' % field
 
     def load_csv(self):
         '''
